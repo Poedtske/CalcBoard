0a60ecfbaea24da09de0fec342c8f3669d16f62f
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2025-02-07 13:58:23 +0100

    GameSelect
    User kan de game electro selecteren van uit de folder games, de scene word dan opgeladen.
    
    reviewd by: Henrique

---
+++ Calcboard/Assets/gameSelector.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class gameSelector : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    //Function to load spefiek games
+    public void OpenScene()
+    {
+        SceneManager.LoadScene("Electro");
+    }
+}
